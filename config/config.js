d1c756d4221ea8660be70e7583229eb41df04e23
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2024-02-01 10:25:56 +0300

    initial commit

---
+++ config/config.js
@@ -0,0 +1,61 @@
+﻿Config({
+    "network-name": "4G فور جي ",
+    "service-number": "733024644",
+    "login-type": "user",
+    "news-line": "...:::: اهلا بكم في شبكة الزين لسنا الوحيدون ولكننا نقدم أفضل الخدمات - أسعار مميزة وباقات تناسب جميع الاحجام - شكرا لاستخدامك خدماتنا ::::...",
+    "input-type": "number",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "enable-hot-cookie": 0,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+    {
+        "price": "100 ريال",
+        "transfer": "200 ميقا",
+        "validity": "يوم"
+    },
+    {
+        "price": "200 ريال",
+        "transfer": "500 ميقا",
+        "validity": "يومين"
+    },
+    {
+        "price": "500 ريال",
+        "transfer": "1500 ميقا",
+        "validity": " أسبوع"
+    },
+    {
+        "price": "1500 ريال",
+        "transfer": "5 قيقا",
+        "validity": " شهر"
+    },
+    {
+        "price": "3000 ريال",
+        "transfer": "12 قيقا",
+        "validity": " شهر"
+    }
+],
+    "sell-points": [
+        {"name":"متوفرة الكروت في جميع البقالات الواقعة ضمن تغطية الشبكة "},
+    ]
+})
+
